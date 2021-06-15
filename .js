4747cc184323f9af6d075db1e943557994ae9220
skai <83657153+Skai-2012@users.noreply.github.com>
2021-06-15 09:30:49 -0400

    Update .js

--- .js
+++ .js
@@ -1,3 +1,7 @@
 $ git remote -v
 > origin  https://github.com/Skai-2012/Cool.git (fetch)
 > origin  https://github.com/Skai-2012/Cool.git (push)
+
+>more fork https$ git remote -v
+> origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (fetch)
+> origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (push)
